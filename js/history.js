943c5144fb21b5155327d63c880977c489b5ea87
LakshyaIndian <121753994+LakshyaIndian@users.noreply.github.com>
2026-04-18 22:15:58 +0530

    Fix double tap + date rollover

--- js/history.js
+++ js/history.js
@@ -1,1 +1,30 @@
-function confirmMala(){let data=getData();let today=getTodayKey();if(!data[today])data[today]=0;data[today]++;saveData(data);updateTodayCount();closeModal();resetTimer()}function updateTodayCount(){let data=getData();let today=getTodayKey();document.getElementById("todayCount").innerText=(data[today]||0)+" malas today"}
+let confirmLock=false;
+
+function confirmMala(){
+  if(confirmLock) return;
+  confirmLock=true;
+
+  let data=getData();
+  let today=getTodayKey();
+
+  if(!data[today]) data[today]=0;
+  data[today]++;
+
+  saveData(data);
+  updateTodayCount();
+  closeModal();
+  resetTimer();
+
+  setTimeout(()=>confirmLock=false,500);
+}
+
+function updateTodayCount(){
+  let data=getData();
+  let today=getTodayKey();
+  document.getElementById("todayCount").innerText=(data[today]||0)+" malas today";
+}
+
+/* date rollover */
+document.addEventListener("visibilitychange",()=>{
+  updateTodayCount();
+});
