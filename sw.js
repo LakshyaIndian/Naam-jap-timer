5e4538c222f182d918588f09a0c0d41183570b26
LakshyaIndian <121753994+LakshyaIndian@users.noreply.github.com>
2026-04-18 22:07:35 +0530

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="mala-cache-v1";self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/index.html","/css/style.css","/js/app.js"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
